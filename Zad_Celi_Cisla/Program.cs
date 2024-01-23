5b69bba326f8e5079af774572f8f57f7a8083146
AHristov99 <157456317+AHristov99@users.noreply.github.com>
2024-01-23 09:33:31 +0200

    Add files via upload

---
+++ Zad_Celi_Cisla/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad_Celi_Cisla
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Vivedi cislo za a=");
+            int a=int.Parse(Console.ReadLine());
+
+            Console.Write("Vivedi cislo za b=");
+            int b = int.Parse(Console.ReadLine());
+
+            int c = a + b + 2*a*b;
+            Console.WriteLine("c=" + c);
+            
+        }
+    }
+}
